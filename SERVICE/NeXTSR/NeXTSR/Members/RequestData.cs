ee6584bc603751baf1fe577eacb3a1fab5f79dbe
Haluk <haluky@live.com>
2016-03-11 01:02:32 +0200

    v2

--- SERVICE/NeXTSR/NeXTSR/Members/RequestData.cs
+++ SERVICE/NeXTSR/NeXTSR/Members/RequestData.cs
@@ -8,10 +8,27 @@ namespace NeXTSR.Members
     public class RequestData
     {
         public long ID { get; set; }
+        public string RequestTitle { get; set; }
+        public string RequestBody { get; set; }
         public string RequestType { get; set; }
         public string RequestUser { get; set; }
         public string RequestVolunteer { get; set; }
-        public string RequestValues { get; set; }
+        //public string RequestValues { get; set; }
+        public string RequestState { get; set; }// Gönderildi , Onaylandı, Rezervedlidi, Alındı
+        public string RequestLocation { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime VerifyTime { get; set; }
+        public DateTime ProcessTime { get; set; }
+        //Erişim Bilgisi
+        public long Latid { get; set; }
+        public long Longt { get; set; }
+        public string AccessID { get; set; }
+        public string AccessPass { get; set; }
+        public string AccessCode { get; set; }
+
+        ///     VEYA
+        ///     ID & PASS GİRİLDİKTEN SONRA VERİLEN 4 HANELİ ŞİFREYE GÖRE
+        ///     Bir Gate Açılarak AV Tamamlanacak
 
     }
 }
